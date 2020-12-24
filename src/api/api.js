3aacb8306fd4003ee2c45e7168c5a91b0e2dd4d5
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2020-12-24 16:57:26 +0300

    ip address tested

--- src/api/api.js
+++ src/api/api.js
@@ -2,14 +2,12 @@ import * as axios from "axios";
 
 const instance =  axios.create({
     baseURL: '',
-
 })
-
 
 export const addressAPI = {
 
     getAddress(remoteIP) {
-        return instance.get(`https://geo.ipify.org/api/v1?apiKey=at_vVCAR5mM9BDdzdl8OP8CDH4B4PxQe&ipAddress=${remoteIP}`)
+        return instance.get(`https://geo.ipify.org/api/v1?apiKey=at_vVCAR5mM9BDdzdl8OP8CDH4B4PxQe&ipAddress=8.8.8.8`)
             .then(response =>{
                 return response
             })
