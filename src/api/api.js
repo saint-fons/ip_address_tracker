cfa642bdedc3a26f0f717941ff6347dbfd07a18e
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2020-12-25 22:23:59 +0300

    axios fidex

--- src/api/api.js
+++ src/api/api.js
@@ -7,7 +7,7 @@ const instance =  axios.create({
 export const addressAPI = {
 
     getAddress(remoteIP) {
-        return instance.get(`https://geo.ipify.org/api/v1?apiKey=at_vVCAR5mM9BDdzdl8OP8CDH4B4PxQe&ipAddress=${remoteIP}`)
+        return axios.get(`https://geo.ipify.org/api/v1?apiKey=at_vVCAR5mM9BDdzdl8OP8CDH4B4PxQe&ipAddress=${remoteIP}`)
             .then(response =>{
                 return response
             })
