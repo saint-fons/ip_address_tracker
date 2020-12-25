ce109cdc6487e4babb13bab5cc517999f9cd6899
Ivan Zhuravlev <i.zhuravlev.wok@gmail.com>
2020-12-25 12:32:55 +0300

    before map added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AddressContainer from "./components/Address/AddressContainer";
+import MapContainer from "./components/Map/MapContainer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
             <AddressContainer />
+            <MapContainer />
         </p>
       </header>
 
---
+++ src/components/Map/MapContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+
+function MapContainer() {
+    return<div>
+
+    </div>
+}
+
+
+export default MapContainer
